fceecf8c6078bcf58a597e67156e3ae28ddb3c64
Amin Karaji <amin.krj@gmail.com>
2018-08-27 16:24:57 +1000

    nested attributes sorted

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-export function sum(a, b) {
-  return a + b;
+import _ from "underscore";
+
+export const railsify = (data, root = null) => {
+  let newObject = {}
+
+  _.each(data, (value, key) => {
+    if(Array.isArray(value)){
+      let items = _.map(value, (item) => {
+        return railsify(item)
+      })
+
+      let nested = { [`${key}_attributes`]: items }
+      newObject = { ...newObject, ...nested }
+      return true;
+    }
+
+    if(typeof value === 'object' && value.constructor === Object){
+      let nested = railsify(value, `${key}_attributes`)
+      newObject = { ...newObject, ...nested }
+      return true;
+    }
+
+    newObject = { ...newObject, [key]: value }
+  })
+
+  if(root){
+    return { [root]: newObject }
+  }else{
+    return newObject
+  }
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,98 @@
-import { sum } from './index'
+import { railsify } from './index'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+it('with primitive data', () => {
+  const article = {
+    id: 1,
+    title: "test"
+  }
+
+  const expected = {
+    id: 1,
+    title: "test"
+  }
+
+  expect(railsify(article)).toMatchObject(expected)
+});
+
+it('with primitive data and root', () => {
+  const article = {
+    id: 1,
+    title: "test"
+  }
+
+  const expected = {
+    article: {
+      id: 1,
+      title: "test"
+    }
+  }
+
+  expect(railsify(article, 'article')).toMatchObject(expected)
+});
+
+
+it('with object', () => {
+
+  const article = {
+    id: 1,
+    user: {
+      id: 1,
+      name: "Tom",
+      role: {
+        id: 1,
+        name: "admin"
+      }
+    }
+  }
+
+  const expected = {
+    id: 1,
+    user_attributes: {
+      id: 1,
+      name: "Tom",
+      role_attributes: {
+        id: 1,
+        name: "admin"
+      }
+    }
+  }
+
+  expect(railsify(article)).toMatchObject(expected)
+});
+
+it('with array', () => {
+
+  const article = {
+    id: 1,
+    user: {
+      id: 1,
+      name: "Tom"
+    },
+    comments: [{
+      id: 1,
+      user: {
+        id: 1,
+        name: "Alex"
+      }
+    }]
+  }
+
+  const expected = {
+    id: 1,
+    user_attributes: {
+      id: 1,
+      name: "Tom"
+    },
+    comments_attributes: [
+      {
+        id: 1,
+        user_attributes: {
+          id: 1,
+          name: "Alex"
+        }
+      }
+    ]
+  }
+
+  expect(railsify(article)).toMatchObject(expected)
 });
